2f2418b56d0c23a1ba74d6713e49ba13aa438d54
zigang93 <tanzigang93@gmail.com>
2019-03-06 13:00:30 +0800

    add utlis and hardcode component

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, ScrollView, Text } from 'react-native';
 
-export default class App extends React.Component {
+import EditableTimer from './components/EditableTimer';
+import ToggleableTimerForm from './components/ToggleableTimerForm';
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.appContainer}>
+
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Timers</Text>
+        </View>
+
+        <ScrollView style={styles.timerList}>
+          <ToggleableTimerForm isOpen={true} /> 
+
+            <EditableTimer
+              id="1"
+              title="Mow the lawn"
+              project="House Chores"
+              elapsed="8986300"
+              isRunning
+            />
+
+            <EditableTimer
+              id="2"
+              title="Bake squash"
+              project="Kitchen Chores"
+              elapsed="3890985"
+              editFormOpen
+            />
+
+        </ScrollView>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles = StyleSheet.create({ 
+  appContainer: {
+    flex: 1, 
   },
+  titleContainer: {
+    paddingTop: 35,
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#D6D7DA',
+  }, 
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  timerList: {
+    paddingBottom: 15,
+  }, 
 });
---
+++ components/EditableTimer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import TimerForm from './TimerForm';
+import Timer from './Timer.js';
+
+export default function EditableTimer({ 
+  id,
+  title,
+  project,
+  elapsed,
+  isRunning,
+  editFormOpen,
+}) 
+
+{
+  // if (editFormOpen) {
+  //   return <TimerForm id={id} title={title} project={project} />;
+  // }
+
+  return ( 
+    <Timer
+      id={id}
+      title={title}
+      project={project}
+      elapsed={elapsed}
+      isRunning={isRunning}
+    />
+  ); 
+}
